c2ef5d4d49c6ae90c2fa4d6fb380b098f63febab
Jordan Paris <hamologist@gmail.com>
2023-07-09 01:32:44 -0400

    Runs rustfmt against easy/fizz-buzz

--- easy/fizz-buzz/src/lib.rs
+++ easy/fizz-buzz/src/lib.rs
@@ -2,7 +2,7 @@ pub struct Solution {}
 
 impl Solution {
     pub fn fizz_buzz(n: i32) -> Vec<String> {
-        let mut result: Vec<String> = vec!();
+        let mut result: Vec<String> = vec![];
 
         for num in 1..=n {
             if num % 3 == 0 && num % 5 == 0 {
@@ -33,13 +33,18 @@ mod test {
     #[test]
     fn example_two() {
         let actual = Solution::fizz_buzz(5);
-        assert_eq!(actual, vec!["1","2","Fizz","4","Buzz"]);
+        assert_eq!(actual, vec!["1", "2", "Fizz", "4", "Buzz"]);
     }
 
     #[test]
     fn example_three() {
         let actual = Solution::fizz_buzz(15);
-        assert_eq!(actual, vec!["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]);
+        assert_eq!(
+            actual,
+            vec![
+                "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz",
+                "13", "14", "FizzBuzz"
+            ]
+        );
     }
 }
-
